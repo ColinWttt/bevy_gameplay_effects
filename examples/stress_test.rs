62156197cb057c7b764a4a365cd1b860534c64e3
Matt Seaton <mattseaton22@gmail.com>
2025-09-13 11:47:36 -0500

    Updated stress test

--- examples/stress_test.rs
+++ examples/stress_test.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 
 use bevy::prelude::*;
+use bevy_hierarchical_tags::prelude::*;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::time::common_conditions::on_timer;
 use bevy::window::PresentMode;
@@ -27,18 +28,23 @@ const VARIANTS: [CharacterStats; 3] = [
     CharacterStats::Strength
 ];
 
-/// Some effects we can use
-struct HealingEffect;
-struct OnFireEffect;
-struct DamageEffect;
-struct DeathEffect;
+#[derive(Resource)]
+struct Tags {
+    on_fire_tag: TagId,
+    healing_tag: TagId,
+}
 
 fn main() {
     let mut app = App::new();
+    let mut tag_registry: TagRegistry<16> = TagRegistry::new();
+    let on_fire_tag = tag_registry.register("Effect.Status.Burning");
+    let healing_tag = tag_registry.register("Effect.Status.Healing");
+    app.insert_resource(tag_registry);
+    app.insert_resource(Tags{ on_fire_tag, healing_tag });
 
     let stacking_behaviors = StackingBehaviors::new()
-        .stack::<OnFireEffect>(StackingPolicy::NoStackingResetDuration) 
-        .stack::<HealingEffect>(StackingPolicy::MultipleEffects(2)); // Can stack up to 2 healing effects
+        .stack(on_fire_tag, StackingPolicy::NoStackingResetDuration) 
+        .stack(healing_tag, StackingPolicy::MultipleEffects(2)); // Can stack up to 2 healing effects
 
     app.add_plugins((
         DefaultPlugins.set(WindowPlugin {
@@ -67,7 +73,8 @@ fn main() {
 
 fn spawn_entities(mut commands: Commands) {
     let active_effects = ActiveEffects::new([
-        GameplayEffect::new::<DeathEffect>(
+        GameplayEffect::new(
+            None,
             CharacterStats::Health,
             EffectMagnitude::Fixed(0.),
             EffectCalculation::LowerBound,
@@ -98,7 +105,8 @@ fn do_some_effects(
     mut commands: Commands,
     entities: Query<Entity, With<ActiveEffects<CharacterStats>>>,
 ) {
-    let damage_effect = GameplayEffect::new::<DamageEffect>(
+    let damage_effect = GameplayEffect::new(
+        None,
         CharacterStats::Health,
         EffectMagnitude::LocalStat(CharacterStats::Strength, StatScalingParams{multiplier: -1.0, ..default()}),
         EffectCalculation::Additive,
@@ -108,7 +116,7 @@ fn do_some_effects(
     for entity in entities {
         // Take some damage
         commands.trigger(AddEffect(AddEffectData::new(
-            entity, damage_effect.clone()
+            entity, damage_effect.clone(), None
         )));
     }
 }
@@ -116,6 +124,7 @@ fn do_some_effects(
 fn check_deaths(
     mut commands: Commands,
     mut events: EventReader<OnBoundsBreached<CharacterStats>>,
+    tags: Res<Tags>,
 ) {
     // Since all entities are receiving the same damage each frame they will all die
     // and fire these events at the same time.  This causes a big fps drop, down to 60 fps for me.
@@ -123,7 +132,8 @@ fn check_deaths(
     // of entities.
 
     // This will give 100 Health/s for 5 seconds
-    let healing_effect = GameplayEffect::new::<HealingEffect>(
+    let healing_effect = GameplayEffect::new(
+        Some(tags.healing_tag),
         CharacterStats::Health,
         EffectMagnitude::Fixed(100.0),
         EffectCalculation::Additive,
@@ -132,7 +142,7 @@ fn check_deaths(
     for event in events.read() {
         if event.0.stat == CharacterStats::Health && event.0.bound == EffectCalculation::LowerBound {
             // Oh no entity died, let's heal him!
-            commands.trigger(AddEffect(AddEffectData::new(event.target_entity, healing_effect.clone())));
+            commands.trigger(AddEffect(AddEffectData::new(event.target_entity, healing_effect.clone(), None)));
         }
     }
 }
